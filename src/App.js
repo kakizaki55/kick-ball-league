912a3f17f5d3a7e42db461bbe927c3552503949a
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-16 11:47:50 -0800

    forgot to make a dev banch, got the home page navlinks to work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './views/TeamList/TeamsList';
+import TeamsList from './views/TeamList/TeamsList';
+import PlayerList from './views/PlayerList/PlayerList';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>kick ball</p>
+      <BrowserRouter>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/teams">Teams</NavLink>
+        <NavLink to="/players">Players</NavLink>
+        <Switch>
+          <Route exact path="/">
+            <h1>Kick Ball Leage HOme Page</h1>
+          </Route>
+          <Route path="/players" component={TeamsList} />
+          <Route path="/player">
+            <PlayerList></PlayerList>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+0;
